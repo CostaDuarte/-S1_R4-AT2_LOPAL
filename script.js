de1f5c63bbfe8539f56958f203fc54fff6e909ad
I1HN <nicolas.d.costa6@edu.senai.br>
2026-04-15 08:55:06 -0300

    :goal_net: implementado verificador se valor inserido é numero e revisão de erros ortograficos.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-numUm = Number(prompt("Me diga um número e eu verei se ele é par"));
-if (numUm % 2 == 0) {
-  alert("o número informado é par");
+numUm = Number(prompt("Me diga um número e eu verei se ele é par."));
+if (isNaN(numUm)) {
+  alert("Você não inseriu um número.");
+} else if (numUm % 2 == 0) {
+  alert("O número informado é par.");
 } else {
-  alert("o número informado é impar");
+  alert("O número informado é impar.");
 }
