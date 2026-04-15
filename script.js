ae4427cebb8a0a4fe992b2bf2cc8ef471ac051d0
I1HN <nicolas.d.costa6@edu.senai.br>
2026-04-15 07:02:02 -0300

    :tada: init: criado o código do exercício 1

---
+++ script.js
@@ -0,0 +1,6 @@
+numUm = Number(prompt("Me diga um número e eu verei se ele é par"));
+if (numUm % 2 == 0) {
+  alert("o número informado é par");
+} else {
+  alert("o número informado é impar");
+}
