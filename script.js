ecbbef15a789f82cdd884aa335bef77e3cfd176e
I1HN <nicolas.d.costa6@edu.senai.br>
2026-04-17 07:30:55 -0300

    :bulb: docs: documentado o que parseInt e isNan fazem

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+//parseInt: converte uma string para um número inteiro
+//usei esse codigo para impedir que o usúario digite números que não são inteiros
 numUm = parseInt(prompt("Me diga um número e eu verei se ele é par."));
+//isNan: envia um sinal booleano verdadeiro quando o valor da variável é texto
+//usei esse codigo para impedir que o usúario digite algo que não é número
 if (isNaN(numUm)) {
   alert("Você não inseriu um número.");
 } else if (numUm % 2 == 0) {
